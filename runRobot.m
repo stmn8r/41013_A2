5b3fa59a13c9c5dcdb1a6b7dcd33ae1dddd0d309
robertattard3 <robert.attard3@gmail.com>
2024-10-25 21:35:52 +1100

    GUI updates

--- runRobot.m
+++ runRobot.m
@@ -1,8 +1,8 @@
 clear all;
 
 % Create an instance of the Assignment1 class
-app = Assignment2();
+self = Assignment2();
 
 % Call the run method
-app = app.initialiseScene();
-app = app.controlLoop();
+self = self.initialiseScene();
+self = self.controlLoop();
