4c2b4c115ad993b7db41b41fd6b990ffbd9f175d
robertattard3 <robert.attard3@gmail.com>
2024-10-29 21:34:52 +1100

    Updated safety features

--- runRobot.m
+++ runRobot.m
@@ -1,8 +1,9 @@
 clear all;
 
 % Create an instance of the Assignment1 class
-self = Assignment2();
-
-% Call the run method
-self = self.initialiseScene();
-self = self.controlLoop();
+% self = Assignment2();
+% 
+ % Call the run method
+% self = self.initialiseScene();
+% self = self.controlLoop();
+GUItest;
