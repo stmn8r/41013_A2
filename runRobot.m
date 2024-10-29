0794833aefa2b76fc2c715d00f829a1345aef994
Liam5347 <liam.s.calder@gmail.com>
2024-10-30 00:47:20 +1100

    change folders

--- runRobot.m
+++
@@ -1,9 +0,0 @@
-clear all;
-
-% Create an instance of the Assignment1 class
-% self = Assignment2();
-% 
- % Call the run method
-% self = self.initialiseScene();
-% self = self.controlLoop();
-GUItest;
